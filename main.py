87eff60c9d44b05210f38c86abff945c9a04c3f5
Matthew Qandil <matthewpqandil@gmail.com>
2021-11-09 19:28:29 +0900

    Datetime Test

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from datetime import datetime as dt
+
 print('hi')
 
 print('this is a change')
+
+
+#testing stuff to try to get github to work // can delete
+current_time = dt.now()
+print(F"The time is {current_time}")
